2b580cb5c1c7c1bab585840ce085a5ccf4c88623
jinwoong96 <wlsdnd921@gmail.com>
2026-07-21 11:33:01 +0900

    사용자 프로필 컬럼 수정

--- app/db/scheme/user_testprofile.py
+++ app/db/scheme/user_testprofile.py
@@ -11,26 +11,14 @@ class UserTestProfileCreate(BaseModel):
     region: Optional[str] = None
     district: Optional[str] = None
     education: Optional[str] = None
-    school_name: Optional[str] = None
-    major: Optional[str] = None
     major_category: Optional[str] = None
-    student_status: Optional[str] = None
-    graduation_year: Optional[int] = None
     employment_status: Optional[str] = None
-    occupation: Optional[str] = None
-    job_seeking: bool = False
-    career_history: Optional[str] = None
+    sme_employment: bool = False
     marital_status: Optional[str] = None
     disability: bool = False
     basic_livelihood: bool = False
     single_parent: bool = False
-    startup_interest: bool = False
-    business_owner: bool = False
-    startup_status: Optional[str] = None
-    company_type: Optional[str] = None
     situation: Optional[str] = None
-    housing_status: Optional[str] = None
-    reason: Optional[str] = None
 
 
 class UserTestProfileRead(UserTestProfileCreate):
